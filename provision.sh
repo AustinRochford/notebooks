15e427dec984adbf933d16eae5bc22484f7ab2d6
AustinRochford <arochford@monetate.com>
2015-04-13 22:00:43 -0400

    Add scikit-learn

--- provision.sh
+++ provision.sh
@@ -16,9 +16,9 @@ pip install numpy scipy
 apt-get install -y libpng12-dev libfreetype6-dev
 pip install matplotlib seaborn bokeh
 
-# Install pandas, patsy, statsmodels, and pymc3
+# Install pandas, patsy, statsmodels, scikit-learn and pymc3
 ln -s /usr/include/freetype2/ft2build.h /usr/include/
-pip install pandas patsy statsmodels
+pip install pandas patsy statsmodels scikit-learn
 pip install --process-dependency-links git+https://github.com/pymc-devs/pymc3
 
 # Install IBash kernel
